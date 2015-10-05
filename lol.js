0a95f2ff13e99ee534102f82e3cf25f5bc53ef93
Vironika Lishtvan <vironika.lishtvan@f1006-21-34182.bmcc.cuny.edu>
2015-10-05 16:31:38 -0400

    testing

---
+++ lol.js
@@ -0,0 +1,16 @@
+var  redX = 55,
+ redY = 75;
+
+var  greenX = 55,
+  greenY = 175;
+
+var blueX = 55,
+ blueY = 275;
+
+var linecolor;
+
+var sY1 = 420;
+var sY2= 440;
+var sY3 = 470;
+var sX = 20;
+var X = 80;
