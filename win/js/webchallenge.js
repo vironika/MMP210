c925058480fafaab7fbd0b4ea027da52ebde4900
Vironika Lishtvan <vironika.lishtvan@f1006-21-34182.bmcc.cuny.edu>
2015-10-19 16:41:34 -0400

    doesn'twork

--- win/js/webchallenge.js
+++ win/js/webchallenge.js
@@ -1,32 +1,63 @@
 function setup() {
-    //create the Canvas
+
 
     createCanvas(windowWidth, windowHeight);
     background(50,56,59);
-var x = 200;
-    var y =100;
-
-drawWindow(){}
-     strokeWeight(10);
-  stroke('black');
-  fill('');
-  rect(x-30,y-30,360,460);
 
 
-  //inside box
-  strokeWeight(10);
-  stroke('white');
-  fill('blue');
-  rect(x,y,300,400);
-
-    strokeWeight(20);
-    stroke('white')
-    line(x+150, y, x+150, y+400);
-    line(x, y+200, x+300, y+200);
 
 
+ var x = 200;
+ var y = 100;
+var gridWidth = width - 100;
+for (var i = 0; i < 100; i++) {
+    console.log(i);
+
+
+ drawWindow(x,y);
+
+
+    x += 120;
+    if (x > gridWidth)
+    {
+        x = 0;
+         y+=270;}
+}
 
 }
+//    fill('blue')
+//    rect(startX, startY, 100,250)
+//    }
+
+
+
+//     strokeWeight(10);
+//  stroke('black');
+//  fill('');
+//  rect(x-30,y-30,360,460);
+//
+//    var x = 200;
+//    var y =100;
+//
+//
+//fanction draw (){}
+//  //inside box
+//
+//    function drawWindow(x, y){}
+//
+//  strokeWeight(10);
+//  stroke('white');
+//  fill('blue');
+//  rect(x,y,300,400);
+//
+//    strokeWeight(20);
+//    stroke('white')
+//    line(x+150, y, x+150, y+400);
+//    line(x, y+200, x+300, y+200);
+//
+//
+
+
 
 //  strokeWeight(4);
 //  stroke('peru');
