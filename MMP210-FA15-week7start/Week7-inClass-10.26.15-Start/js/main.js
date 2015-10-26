d0f0a12784eadb0b0cef26f1fc057283e14e0c25
Vironika Lishtvan <vironika.lishtvan@f1006-21-34182.bmcc.cuny.edu>
2015-10-26 14:49:10 -0400

    array

---
+++ MMP210-FA15-week7start/Week7-inClass-10.26.15-Start/js/main.js
@@ -0,0 +1,17 @@
+//global variables
+
+function setup(){
+    //create the canvas. put into a variable to associate with a element
+    var cnv = createCanvas(800,3000);
+    cnv.parent("windows");
+
+    var myArray = ["hello", 3, "bye"];
+
+    console.log(myArray[0]);
+    console.log(myArray[1]);
+    console.log(myArray[2]);
+     console.log(myArray);
+     console.log("myArray", myArray);
+
+}//end setup
+
