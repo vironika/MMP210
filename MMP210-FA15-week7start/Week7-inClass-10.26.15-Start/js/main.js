0258e12320f1a78261407392dc8f634d1c4fb100
Vironika Lishtvan <vironika.lishtvan@f1006-21-34182.bmcc.cuny.edu>
2015-10-26 17:09:21 -0400

    not working

--- MMP210-FA15-week7start/Week7-inClass-10.26.15-Start/js/main.js
+++ MMP210-FA15-week7start/Week7-inClass-10.26.15-Start/js/main.js
@@ -1,20 +1,52 @@
 //global variables
 
+ var myArray = [];
+ this.r = 20;
+
+    this.d = this.r*2;
+
+
+function Bubble(){
+
+
+    this.x = random(width);
+    this.y = random(height);
+    this.r = 20;
+
+    this.d = this.r*2;
+    this.drawBubble = function(){
+
+    ellipse(this.x, this.y, this.d, this.d );}
+
+}
+
+
+this.changeColor = function(){
+
+    fill ("orange");
+    this.drawBubble();
+}
+
 function setup(){
     //create the canvas. put into a variable to associate with a element
-    var cnv = createCanvas(800,3000);
+    var cnv = createCanvas(800,500);
     cnv.parent("windows");
+//
 
-    var myArray = [];
-    for(var i=0; i<10000; i++){myArray[i] = i;
-                           }
-    console.log(myArray);
 
-    for(var i=0; i < myArray.length; i++)
-    {myArray[i] += 10;
+    for(var i=0; i < 100; i++)
+    {
 
-    }
-    console.log(myArray);
+        myArray[i] = new Bubble();
+        myArray[i] .drawBubble();
+         }
+//    console.log(myArray);
+//
+//    for(var i=0; i < myArray.length; i++)
+//    {myArray[i] += 10;}
+//
+//    }
+//    console.log(myArray);
 
 //    console.log(myArray[0]);
 //    console.log(myArray[1]);
@@ -24,3 +56,18 @@ function setup(){
 
 }//end setup
 
+function mousePressed(){
+     var d;
+    for(var i=0; i < myArray.length; i++){
+    d = dist(mouseX, mouseY, myArray[i].x, myArray[i].y);
+     if(d < myArray[i].r) {
+//     r = random(255);
+//     g = random(255);
+//     b = random(255);
+
+   myArray[i].changeColor();
+
+    console.log("mouse Pressed");
+      console.log(myArray);
+}
+}}
