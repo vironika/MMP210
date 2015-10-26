182746a98693ff14cf27d3eefa1f674483a2b2bb
Vironika Lishtvan <vironika.lishtvan@f1006-21-34182.bmcc.cuny.edu>
2015-10-26 15:07:21 -0400

    loop array

--- MMP210-FA15-week7start/Week7-inClass-10.26.15-Start/js/main.js
+++ MMP210-FA15-week7start/Week7-inClass-10.26.15-Start/js/main.js
@@ -5,13 +5,22 @@ function setup(){
     var cnv = createCanvas(800,3000);
     cnv.parent("windows");
 
-    var myArray = ["hello", 3, "bye"];
+    var myArray = [];
+    for(var i=0; i<10000; i++){myArray[i] = i;
+                           }
+    console.log(myArray);
 
-    console.log(myArray[0]);
-    console.log(myArray[1]);
-    console.log(myArray[2]);
-     console.log(myArray);
-     console.log("myArray", myArray);
+    for(var i=0; i < myArray.length; i++)
+    {myArray[i] += 10;
+
+    }
+    console.log(myArray);
+
+//    console.log(myArray[0]);
+//    console.log(myArray[1]);
+//    console.log(myArray[2]);
+//     console.log(myArray);
+//     console.log("myArray", myArray);
 
 }//end setup
 
